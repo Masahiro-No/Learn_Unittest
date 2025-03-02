01c35fb71e2f62c03fd414895ac2b8787861353b
Masahiro | まさひろ <krittanaiphi@gmail.com>
2025-03-02 15:27:08 +0700

    solve alternatingCharacters problem

---
+++ coe_number/alternatingCharacters.py
@@ -0,0 +1,22 @@
+def alternatingCharacters(s):
+    num=0
+    for i in range(1,len(s)):
+        if s[i-1]==s[i]:
+            num+=1
+    return num
+
+print(alternatingCharacters("AAAA")) # ควรได้ 3
+print(alternatingCharacters("BBBBB")) # ควรได้ 4
+print(alternatingCharacters("ABABABAB")) # ควรได้ 0
+print(alternatingCharacters("AAABBB")) # ควรได้ 4
+print(alternatingCharacters("AAABBBAABB")) # ควรได้ 6
+print(alternatingCharacters("AABBAABB")) # ควรได้ 4
+print(alternatingCharacters("ABABABAA")) # ควรได้ 1
+print(alternatingCharacters("XYZXXQWE")) # ควรได้ 4
+print(alternatingCharacters("A")) # ควรได้ 0
+print(alternatingCharacters("")) # ควรได้ 0
+print(alternatingCharacters("AB")) # ควรได้ 0
+print(alternatingCharacters("AA")) # ควรได้ 1
+print(alternatingCharacters("กกก")) # ควรได้ 2
+print(alternatingCharacters(".,.,")) # ควรได้ 0
+print(alternatingCharacters("       ")) # ควรได้ 6
